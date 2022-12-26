9c83f26b14da767fd7aa73242eac56dad403cec9
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2022-12-26 04:34:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-function right()
-{ console.log (" ") };
+nes (20 sloc)  567 Bytes
 
-function wrong() 
-{ alert("Not found") };
-
-function key()
-{ var first = document.getElementById("cost");
-var second = document.getElementById("number");
-var lt = document.getElementById("outcome");
-var last = first.value * second.value;
-if (!Number.isNaN(last) && first > 0 && second > 0)
-{ lt.value = last;
-right(); }
-else
-{ lt.value = "ERROR"
-wrong(); }
+function falseD() {
+    alert("ERROR: Incorrect data");
+}
+function correct() {
+    console.log("Correct data");
 }
 
-window.addEventListener ('DOMContentLoaded', function (event) {
-console.log("DOM is correct");
-let button = document.getElementById("pushbutton");
-button.addEventListener('key', key);
+function click1() {
+    var f1 = document.getElementById("price");
+    var f2 = document.getElementById("count");
+    var r = document.getElementById("result");
+    let total = f1.value * f2.value;
+    if (!Number.isNaN(total) && f1.value > 0 && f2.value > 0) {
+        r.innerHTML = total;
+        correct();
+    }
+    else falseD();
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+    console.log("DOM fully loaded and parsed");
 })
